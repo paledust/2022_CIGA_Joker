7abc16ff9caf3db7fffb02f684e2b387ababa3bb
paledust <paledust@hotmail.com>
2022-06-25 02:21:32 +0800

    多放了一些箱子，以测试功能

--- Assets/DevelopBasic/Util/Main.cs
+++ Assets/DevelopBasic/Util/Main.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 //Please make sure "Main" is excuted before every custom script but after "GameManager"
 public class Main : MonoBehaviour
 {
+    [SerializeField] private Player player1;
+    [SerializeField] private Player player2;
     protected virtual void Awake(){
         GameManager.mainCam = Camera.main;
-        // GameManager.
+        GameManager.player1 = player1;
+        GameManager.player2 = player2;
     }
 }
