506e128e613fd6ab44b2e4305f27931301bc22b1
paledust <paledust@hotmail.com>
2022-06-24 22:08:02 +0800

    更新box

---
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] private Collider2D m_collider;
+    void Awake()=>m_collider = GetComponent<Collider2D>();
+    public virtual void OnInteract(){}
+    public void DisableTrigger(){m_collider.enabled = false;}
+    public void EnableTrigger(){m_collider.enabled = true;}
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,22 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float MoveSpeed = 1;
-    private Vector3 direction;
+    [SerializeField] private float detectRange = 0.2f;
+    private Vector2 direction;
+    private Vector3 facingDirection = Vector3.up;
+    private Rigidbody2D m_rigid;
+    private Ray2D ray;
+    void Awake(){
+        m_rigid = GetComponent<Rigidbody2D>();
+    }
     void Update(){
-        transform.position += direction * MoveSpeed * Time.deltaTime;
+    }
+    void FixedUpdate(){
+        m_rigid.position += direction * MoveSpeed * Time.fixedDeltaTime;
     }
     void OnMove(InputValue value){
         direction = value.Get<Vector2>();
+        if(direction != Vector2.zero) facingDirection = direction;
     }
     void OnPutIn(InputValue value){
         //To Do:检测有无可以放入的对象
@@ -23,5 +33,9 @@ public class Player : MonoBehaviour
     void OnAttack(InputValue value){
         //To Do:检测有无可以攻击的对象
         Debug.Log("Attack");
+    }
+    void OnDrawGizmos(){
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, facingDirection*detectRange);
     }
 }
