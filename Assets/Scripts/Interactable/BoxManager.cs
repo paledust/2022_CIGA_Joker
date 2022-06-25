b62f350e51d87e379d373615943e72254634ab48
GreyDove <1608369704@qq.com>
2022-06-25 15:23:43 +0800

    Broadcast

--- Assets/Scripts/Interactable/BoxManager.cs
+++ Assets/Scripts/Interactable/BoxManager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class BoxManager : MonoBehaviour
 {
     private List<Box> allBoxes;
-    public int putNum = 3;
+    [SerializeField]private int num = 3;
+    public int PutNum {
+        get => Mathf.Max(num, allBoxes.Count);
+    }
     // private List<Box> emptyBoxes;
     private void OnEnable() {
         EventHandler.E_OnPutCoins += PutCoins; 
@@ -18,17 +21,16 @@ public class BoxManager : MonoBehaviour
     private void Start() {
         allBoxes = new List<Box>(GetComponentsInChildren<Box>());
     }
-    
+
     private void PutCoins() 
     {
         for (int i = allBoxes.Count - 1; i > 0; i--)
         {
-            int t = Random.Range(0, i);
-            (allBoxes[i], allBoxes[t]) = (allBoxes[t], allBoxes[i]);
+            int rand = Random.Range(0, i);
+            (allBoxes[i], allBoxes[rand]) = (allBoxes[rand], allBoxes[i]);
         }
-
-        Debug.Log($"有{Mathf.Min(putNum, allBoxes.Count)}个盒子新放入了一枚糖果！！！！快来拿啊！！！");
-        for (int i = 0; i < System.Math.Min(putNum, allBoxes.Count); i++)
+        Debug.Log($"有{PutNum}个盒子新放入了一枚糖果！！！！快来拿啊！！！");
+        for (int i = 0; i < PutNum; i++)
         {
             allBoxes[i].PutInCoin(1);
         }
