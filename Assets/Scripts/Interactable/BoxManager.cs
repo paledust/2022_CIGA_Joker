8412d24ae4122d0488b26322aa25f303b0ea297b
GreyDove <1608369704@qq.com>
2022-06-25 13:25:54 +0800

    BoxManagerFixedEvent

--- Assets/Scripts/Interactable/BoxManager.cs
+++ Assets/Scripts/Interactable/BoxManager.cs
@@ -7,11 +7,18 @@ public class BoxManager : MonoBehaviour
     private List<Box> allBoxes;
     public int putNum = 3;
     // private List<Box> emptyBoxes;
-    private void Start() {
-        allBoxes = new List<Box>(GetComponentsInChildren<Box>());
-        EventHandler.E_OnPutCoins += PutCoins; //最好写在OnEnable以及OnDisable里面，方便注册以及注销delegate。否则在切换场景或者游戏结束时容易出现内存泄漏。
+    private void OnEnable() {
+        EventHandler.E_OnPutCoins += PutCoins; 
     }
 
+    private void OnDisable() {
+        EventHandler.E_OnPutCoins -= PutCoins; 
+    }
+    
+    private void Start() {
+        allBoxes = new List<Box>(GetComponentsInChildren<Box>());
+    }
+    
     private void PutCoins() 
     {
         for (int i = allBoxes.Count - 1; i > 0; i--)
