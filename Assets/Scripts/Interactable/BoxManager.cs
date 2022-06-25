1b43506d20731b96e6297655b3ec5f967748811e
GreyDove <1608369704@qq.com>
2022-06-25 12:28:59 +0800

    BoxManager

---
+++ Assets/Scripts/Interactable/BoxManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxManager : MonoBehaviour
+{
+    private List<Box> allBoxes;
+    public int putNum = 3;
+    // private List<Box> emptyBoxes;
+    private void Start() {
+        allBoxes = new List<Box>(GetComponentsInChildren<Box>());
+    }
+
+    private void PutCoins() 
+    {
+        for (int i = allBoxes.Count - 1; i > 0; i--)
+        {
+            int t = Random.Range(0, i);
+            (allBoxes[i], allBoxes[t]) = (allBoxes[t], allBoxes[i]);
+        }
+        
+        for (int i = 0; i < System.Math.Max(putNum, allBoxes.Count); i++)
+        {
+            allBoxes[i].PutInCoin(1);
+        }
+    }
+    
+}
