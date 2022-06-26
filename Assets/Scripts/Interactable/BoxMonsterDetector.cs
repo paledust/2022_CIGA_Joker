730db7788b28f8c52b554f689ec3f825d1a1bf38
paledust <paledust@hotmail.com>
2022-06-26 11:29:31 +0800

    移除一些item

---
+++ Assets/Scripts/Interactable/BoxMonsterDetector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMonsterDetector : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator; 
+    private void OnTriggerEnter2D(Collider2D other) {
+
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        
+    }
+}
